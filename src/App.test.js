4e7c9d956b0692ef64d9b026543e629c727d13df
kiken-eng <kanyecope@gmail.com>
2022-12-30 20:05:18 +0300

    update the test run command to be able to exit th terminal on completion

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import { it } from 'eslint/lib/rule-tester/rule-tester';
-import ReactDom from 'react-dom';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/reload/i);
   expect(linkElement).toBeInTheDocument();
-});
-
-it('renders without crushing', () => {
-  const div = document.createElement('div');
-  ReactDom.render(<App />, div);
-  ReactDom.unmountComponentAtNode(div);
 });
